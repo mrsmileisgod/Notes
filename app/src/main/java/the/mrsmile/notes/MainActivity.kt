ec777d355ca3d18cd9642c32e7d7315555adf67c
MrSmile <mrsmileisgod@gmail.com>
2021-10-12 18:49:21 +0530

    Changed UI

--- app/src/main/java/the/mrsmile/notes/MainActivity.kt
+++ app/src/main/java/the/mrsmile/notes/MainActivity.kt
@@ -20,9 +20,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var database: DatabaseReference
-    private lateinit var databaseGet: DatabaseReference
-    lateinit var recyclerView: RecyclerView
-    private lateinit var list: ArrayList<Items>
+
 
     @SuppressLint("UseCompatLoadingForDrawables")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,12 +30,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        list = arrayListOf()
-        getData()
 
-        recyclerView = binding.recyclerView
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.hasFixedSize()
         binding.btnLongNote.setOnClickListener {
 
             val title = getTitlee()
@@ -54,7 +47,6 @@ class MainActivity : AppCompatActivity() {
             binding.eTtitle.text = null
 
         }
-        supportActionBar?.setBackgroundDrawable(getDrawable(R.color.primary))
     }
 
     private fun writeData(title: String, desc: String) {
@@ -70,38 +62,6 @@ class MainActivity : AppCompatActivity() {
         return binding.eTdesc.text.toString()
     }
 
-    private fun getData() {
-
-        databaseGet = FirebaseDatabase.getInstance().getReference("notes")
-        databaseGet.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-
-                if (snapshot.exists()) {
-                    for (noteSnapshot in snapshot.children) {
-
-                        val note = noteSnapshot.getValue<Items>()
-                        Log.d("ListData", noteSnapshot.toString())
-                        if (!list.contains(note)) {
-                            list.add(0, note!!)
-                        }
-                    }
-                    recyclerView.adapter = RecyclerAdapter(list)
-                }
-            }
-
-
-            override fun onCancelled(error: DatabaseError) {
-                Snackbar.make(binding.root, "Data Fetching Cancelled !", Snackbar.LENGTH_SHORT)
-                    .show()
-            }
-        })
-
-//        if(databaseGet.)
-        databaseGet.child("notes").child(getTitlee()).get().addOnSuccessListener {
-            binding.progressBar.visibility = View.GONE
-            binding.tVProgressBar.visibility = View.GONE
-        }
-
-    }
 
 }
+
---
+++ app/src/main/java/the/mrsmile/notes/MainActivity2.kt
@@ -0,0 +1,69 @@
+package the.mrsmile.notes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.getValue
+import the.mrsmile.notes.databinding.ActivityMain2Binding
+
+class MainActivity2 : AppCompatActivity() {
+    lateinit var recyclerView: RecyclerView
+    private lateinit var binding: ActivityMain2Binding
+    private lateinit var list: ArrayList<Items>
+    private lateinit var databaseGet: DatabaseReference
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.bottomNavBar.setItemSelected(R.id.menu_home, true)
+
+        list = arrayListOf()
+        getData()
+
+
+        recyclerView = binding.recyclerMain
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.hasFixedSize()
+    }
+
+    private fun getData() {
+
+        databaseGet = FirebaseDatabase.getInstance().getReference("notes")
+        databaseGet.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                if (snapshot.exists()) {
+                    for (noteSnapshot in snapshot.children) {
+
+                        val note = noteSnapshot.getValue<Items>()
+                        Log.d("ListData", noteSnapshot.toString())
+                        if (!list.contains(note)) {
+                            list.add(0, note!!)
+                        }
+                    }
+                    recyclerView.adapter = RecyclerAdapter(list)
+                }
+            }
+
+
+            override fun onCancelled(error: DatabaseError) {
+                Snackbar.make(binding.root, "Data Fetching Cancelled !", Snackbar.LENGTH_SHORT)
+                    .show()
+            }
+        })
+
+////        if(databaseGet.)
+        databaseGet.child("notes").get().addOnCompleteListener {
+            binding.progressBar.visibility = View.GONE
+            binding.tVProgressBar.visibility = View.GONE
+        }
+
+    }
+}
