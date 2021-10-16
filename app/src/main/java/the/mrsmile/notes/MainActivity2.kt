f8a72618c1ca6f5e63c15f92a356c5c9316f7399
MrSmile <mrsmileisgod@gmail.com>
2021-10-16 19:15:56 +0530

    Changed bottom navbar icons

--- app/src/main/java/the/mrsmile/notes/MainActivity2.kt
+++ app/src/main/java/the/mrsmile/notes/MainActivity2.kt
@@ -14,10 +14,7 @@ import com.google.firebase.database.ktx.getValue
 import the.mrsmile.notes.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
-    lateinit var recyclerView: RecyclerView
     private lateinit var binding: ActivityMain2Binding
-    private lateinit var list: ArrayList<Items>
-    private lateinit var databaseGet: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMain2Binding.inflate(layoutInflater)
@@ -27,63 +24,27 @@ class MainActivity2 : AppCompatActivity() {
         currentFragment(HomeFragment())
         binding.bottomNavBar.setItemSelected(R.id.menu_home, true)
         binding.bottomNavBar.setOnItemSelectedListener {
-            when(binding.bottomNavBar.getSelectedItemId()) {
-                R.id.menu_home -> { supportFragmentManager.beginTransaction()
-                    .replace(R.id.fragment, HomeFragment()).commit()
-                    
-            }
-                R.id.menu_search -> supportFragmentManager.beginTransaction().replace(R.id.fragment,SearchFragment()).commit()
-                R.id.menu_feedback -> supportFragmentManager.beginTransaction().replace(R.id.fragment,FeedbackFragment()).commit()
+            when (binding.bottomNavBar.getSelectedItemId()) {
+                R.id.menu_home -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment, HomeFragment()).commit()
+
+                }
+                R.id.menu_search -> supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment, SearchFragment()).commit()
+
+                R.id.menu_feedback -> supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment, FeedbackFragment()).commit()
 
             }
         }
-//
-//        list = arrayListOf()
-//        getData()
-//
-//
-//        recyclerView = binding.recyclerMain
-//        recyclerView.layoutManager = LinearLayoutManager(this)
-//        recyclerView.hasFixedSize()
+
     }
 
-    private fun currentFragment(fragment : Fragment){
+    private fun currentFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction().apply {
-            replace(R.id.fragment,fragment).commit()
+            replace(R.id.fragment, fragment).commit()
         }
     }
 
-//    private fun getData() {
-//
-//        databaseGet = FirebaseDatabase.getInstance().getReference("notes")
-//        databaseGet.addValueEventListener(object : ValueEventListener {
-//            override fun onDataChange(snapshot: DataSnapshot) {
-//
-//                if (snapshot.exists()) {
-//                    for (noteSnapshot in snapshot.children) {
-//
-//                        val note = noteSnapshot.getValue<Items>()
-//                        Log.d("ListData", noteSnapshot.toString())
-//                        if (!list.contains(note)) {
-//                            list.add(0, note!!)
-//                        }
-//                    }
-//                    recyclerView.adapter = RecyclerAdapter(list)
-//                }
-//            }
-//
-//
-//            override fun onCancelled(error: DatabaseError) {
-//                Snackbar.make(binding.root, "Data Fetching Cancelled !", Snackbar.LENGTH_SHORT)
-//                    .show()
-//            }
-//        })
-//
-//////        if(databaseGet.)
-//        databaseGet.child("notes").get().addOnCompleteListener {
-//            binding.progressBar.visibility = View.GONE
-//            binding.tVProgressBar.visibility = View.GONE
-//        }
-//
-//    }
 }
